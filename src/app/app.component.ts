cee1ad68a38bdb1aca215917af0ed4e44427d05b
mojial <mojial@floridauniversitaria.es>
2024-02-29 13:59:02 +0100

    diseño de header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+import { MainComponent } from "./components/main/main.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, HeaderComponent, MainComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'TFG';
