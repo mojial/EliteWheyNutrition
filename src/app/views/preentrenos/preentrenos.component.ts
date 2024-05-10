0eaf0dbe2e5b4ee1a02cfd33fc6c34005e2878d7
mojial <mojial@floridauniversitaria.es>
2024-05-10 08:55:51 +0200

    arreglos de buscador

--- src/app/views/preentrenos/preentrenos.component.ts
+++ src/app/views/preentrenos/preentrenos.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CardComponent } from "../../components/card/card.component";
 import { UserService } from '../../services/user.service';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-preentrenos',
     standalone: true,
     templateUrl: './preentrenos.component.html',
     styleUrl: './preentrenos.component.css',
-    imports: [CardComponent]
+    imports: [CardComponent,FormsModule]
 })
 export class PreentrenosComponent {
 
@@ -28,5 +29,72 @@ export class PreentrenosComponent {
         })
         .catch(error => console.log(error));
       }
+
+      searchQuery: string = '';
+  
+  searchCategory() {
+    if (this.searchQuery.trim() !== '') {
+        let category: string = '';
+
+        switch (true) {
+            case this.searchQuery.toLowerCase().includes('proteina') || this.searchQuery.toLowerCase().includes('prote') 
+            || this.searchQuery.toLowerCase().includes('whey') 
+            || this.searchQuery.toLowerCase().includes('protein'):
+                category = 'proteina';
+                break;
+            case this.searchQuery.toLowerCase().includes('creatine') || this.searchQuery.toLowerCase().includes('creatina')
+            || this.searchQuery.toLowerCase().includes('crea')
+            || this.searchQuery.toLowerCase().includes('monohidratada'):
+                category = 'creatine';
+                break;
+            case this.searchQuery.toLowerCase().includes('aminoacidos') || this.searchQuery.toLowerCase().includes('amino')
+            || this.searchQuery.toLowerCase().includes('amin') || this.searchQuery.toLowerCase().includes('bcaas')
+            || this.searchQuery.toLowerCase().includes('bcaa'):
+                category = 'aminoacidos';
+                break;
+            case this.searchQuery.toLowerCase().includes('hidratos') || this.searchQuery.toLowerCase().includes('mass')
+            || this.searchQuery.toLowerCase().includes('gainer') || this.searchQuery.toLowerCase().includes('hidra'):
+                category = 'hidratos';
+                break;
+            case this.searchQuery.toLowerCase().includes('preentrenos') || this.searchQuery.toLowerCase().includes('preworkout')
+            || this.searchQuery.toLowerCase().includes('pree') || this.searchQuery.toLowerCase().includes('pre') 
+            || this.searchQuery.toLowerCase().includes('pre-workout') || this.searchQuery.toLowerCase().includes('prework')
+            || this.searchQuery.toLowerCase().includes('work') || this.searchQuery.toLowerCase().includes('entreno'):
+                category = 'preentrenos';
+                break;
+            case this.searchQuery.toLowerCase().includes('controldepeso') || this.searchQuery.toLowerCase().includes('control')
+            || this.searchQuery.toLowerCase().includes('mantener') || this.searchQuery.toLowerCase().includes('peso'):
+                category = 'controldepeso';
+                break;
+            case this.searchQuery.toLowerCase().includes('barritas') || this.searchQuery.toLowerCase().includes('barrita')
+            || this.searchQuery.toLowerCase().includes('bar') || this.searchQuery.toLowerCase().includes('chocolatina'):
+                category = 'barritas';
+                break;
+            case this.searchQuery.toLowerCase().includes('vitaminasminerales') || this.searchQuery.toLowerCase().includes('vitaminas')
+            || this.searchQuery.toLowerCase().includes('minerales') || this.searchQuery.toLowerCase().includes('vitamin')
+            || this.searchQuery.toLowerCase().includes('mineral'):
+                category = 'vitaminasminerales';
+                break;
+            case this.searchQuery.toLowerCase().includes('quemagrasas') || this.searchQuery.toLowerCase().includes('quema')
+            || this.searchQuery.toLowerCase().includes('grasas') || this.searchQuery.toLowerCase().includes('grasa')
+            || this.searchQuery.toLowerCase().includes('burner') || this.searchQuery.toLowerCase().includes('fat burner'):
+                category = 'quemagrasas';
+                break;
+            case this.searchQuery.toLowerCase().includes('intraentrenos') || this.searchQuery.toLowerCase().includes('intra')
+            || this.searchQuery.toLowerCase().includes('intra entreno') || this.searchQuery.toLowerCase().includes('intra entrenos'):
+                category = 'intraentrenos';
+                break;
+            default:
+                category = '';
+                break;
+        }
+
+        if (category !== '') {
+            this.router.navigate(['/', category]);
+        } else {
+            this.router.navigate(['/home']);
+        }
+    }
+}
 
 }
